8c3c1addd06a772cd48377e1fa5902c0110238ff
jakecoltman <jakecoltman@sky.com>
2016-03-23 18:19:52 +0000

    Added ability to present log entry
    Post to slack channel

--- Connections/Presentation/Slack.py
+++ Connections/Presentation/Slack.py
@@ -1,6 +1,11 @@
+import requests
 
 class Slack:
 
     def __init__(self, token: str, channel_name: str):
         self.token = token
         self.channel_name = channel_name
+        self.base_query = r"https://slack.com/api/chat.postMessage?token={0}&channel=%23{1}&text={{0}}&pretty=1".format(self.token, self.channel_name)
+
+    def present_entry(self, entry: str):
+        requests.post(self.base_query.format(entry))
