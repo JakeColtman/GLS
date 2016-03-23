2f1e64d002eafd6de0569f6353870c7cbd44c5e3
jakecoltman <jakecoltman@sky.com>
2016-03-23 19:12:36 +0000

    Added add row ability
    Append to log file

--- LogFile.py
+++ LogFile.py
@@ -11,3 +11,7 @@ class LogFile:
             lines = fileOpen.readlines()
             lines = [LogEntry(x.split(",")) for x in lines[1:]]
             return lines
+
+    def add_row(self, log_entry: LogEntry) -> bool:
+        with open(self.file_name, "a") as fileOpen:
+            fileOpen.writelines(["{0},{1},{2}".format(log_entry.node_id, log_entry.log_id, log_entry.message)])
