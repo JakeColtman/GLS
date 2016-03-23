6279b7aaaaa715159e6d05b9b111bc62898224cf
jakecoltman <jakecoltman@sky.com>
2016-03-23 18:37:16 +0000

    Return log entry class

--- LogFile.py
+++ LogFile.py
@@ -1,11 +1,13 @@
+from typing import List
+from LogEntry import LogEntry
 
 class LogFile:
 
     def __init__(self, file_name):
         self.file_name = file_name
 
-    def get_rows(self):
+    def get_rows(self) -> List[LogEntry]:
         with open(self.file_name, "r") as fileOpen:
             lines = fileOpen.readlines()
-            lines = [x.split(",") for x in lines]
+            lines = [LogEntry(x.split(",")) for x in lines]
             return lines
