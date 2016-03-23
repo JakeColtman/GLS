592ed46892b2162a159ddae247deb65bae7a7b77
jakecoltman <jakecoltman@sky.com>
2016-03-23 18:14:47 +0000

    Added basic initialization
    Finds next available id and assigns it to self

---
+++ LogNode.py
@@ -0,0 +1,5 @@
+
+class LogNode:
+
+    def __init__(self, node_id: int):
+        self.node_id = node_id
--- process.py
+++ process.py
@@ -1,3 +1,12 @@
+from LogNode import LogNode
 
+FILE_NAME = "logs.csv"
 
-FILE_NAME = ""
+def initialize():
+    with open(FILE_NAME, "r") as fileOpen:
+        lines = fileOpen.readlines()
+        lines = [x.split(",") for x in lines]
+        new_node_id = max([x[0] for x in lines[1:]])
+        return LogNode(new_node_id)
+
+print(initialize())
