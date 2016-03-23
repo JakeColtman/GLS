1d1975ef346e97f3fe4d65c282fe35ad82b923b3
jakecoltman <jakecoltman@sky.com>
2016-03-23 19:06:27 +0000

    Testing import method
    Looking at using multiprocessing

--- LogNode.py
+++ LogNode.py
@@ -1,5 +1,14 @@
+from multiprocessing.connection import Listener
+
 
 class LogNode:
-
     def __init__(self, node_id: int):
         self.node_id = node_id
+
+    def start(self):
+        address = ('localhost', 6000)
+        listener = Listener(address)
+        conn = listener.accept()
+        while True:
+            msg = conn.recv()
+            print(msg)
